067706396215eb3b4e710b41cb60b22c0db835ad
ng-wl <weiliangng23@yahoo.com>
2023-10-15 17:20:32 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /home/user/Desktop/ICT3203_Lab/8/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+ip a
+docker run -d -p 80:80 --name my-apache-php-app -v https://github.com/ng-wl/jenkins-php-selenium-test/tree/master/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 

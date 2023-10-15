8bc97b834d4be44b0e464f11041703bff61ceaab
ng-wl <weiliangng23@yahoo.com>
2023-10-15 16:47:30 +0800

    Update AppTest.java

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "127.0.0.1";
+	String url = "192.168.80.145";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";

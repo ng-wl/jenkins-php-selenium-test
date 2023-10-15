ce58a3c56ab33f59d6c67995833e105af58474ee
ng-wl <weiliangng23@yahoo.com>
2023-10-15 19:39:12 +0800

    changed deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /home/user/Desktop/ICT3203_Lab/8/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+
+## Doesn't work!!!
+#docker run -d -p 80:80 --name my-apache-php-app -v /home/Desktop/ICT3203_Lab/8/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+
+## Workaround. /home/Desktop/ICT3203_Lab/8/jenkins-php-selenium-test/src/ is inside the jenkins-blueocean container, which is mapped to the physical machine.
+docker run -d -p 80:80 --name my-apache-php-app php:7.2-apache
+docker cp /home/Desktop/ICT3203_Lab/8/jenkins-php-selenium-test/src/. my-apache-php-app:/var/www/html
+
 sleep 1
 set +x
 
